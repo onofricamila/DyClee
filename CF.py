368a2f69cb01fd45e24fdcbdd9635676fef9fad6
Camila Onofri <onofricamila2@gmail.com>
2019-09-11 15:16:23 -0300

    CF class added, no habia guardado la clase antes

--- CF.py
+++ CF.py
@@ -11,9 +11,15 @@ class CF:
     
     
     def __init__(self, n, LS, SS, tl, ts, D):
+        # number of elements
         self.n = n
+        # vector containing the linear sum of each feature over the n objects
         self.LS = LS
+        # the square sum of features over the n objects
         self.SS = SS
+        # time when the last object was assigned to the μ cluster
         self.tl = tl
+        # time when the μ cluster was created
         self.ts = ts
+        # the μ cluster density
         self.D = D
