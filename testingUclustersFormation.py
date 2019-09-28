d8bf04f094a0a242d30fb46b025958eb22b446ff
Camila Onofri <onofricamila2@gmail.com>
2019-09-28 13:36:11 -0300

    empezamos con las pruebas de cluters formation en s2, pasa la primera

--- testingUclustersFormation.py
+++ testingUclustersFormation.py
@@ -13,6 +13,7 @@ from multiprocessing import Process, Queue
 s1ToS2ComQueue = Queue()  # s1 will write to s2 there
 s2ToS1ComQueue = Queue()  # s2 will write to s1 there
 
+
 rs = 0.06 # --> s = 0.06 * {2 - (-2)} = 0.06 * 4 = 0.24
 # --> hyperbox size per feature = 0.24 / 2 = 0.12
 
@@ -37,33 +38,36 @@ uC4 = uCluster(rs, d4)
 d5 = [0, 1]
 uC5 = uCluster(rs, d5)
 
+# uc6
+d6 = [0.15, 1]
+uC6 = uCluster(rs, d6)
 
-# TEST 1°
+
+
+# TESTING IS DIRECTECTLY CONNECTED U CLUSTER METHOD --> all tests passed :)
+# TEST 1°: uC2 llega a ser directly conn con uC1
 print(uC1.isDirectlyConnectedWith(uC2, 0)) # --> true
 
-# TEST 2°
+# TEST 2°: uC3 por 1 en ambas dimensiones llega a ser directly conn con uC1
 print(uC1.isDirectlyConnectedWith(uC3, 0)) # --> true
 
-# TEST 3°
+# TEST 3°: uC4 por 1 en ambas dimensiones no llega a ser directly conn con uC1
 print(uC1.isDirectlyConnectedWith(uC4, 0)) # --> false
 
-# TEST 4°
+# TEST 4°: uC5 es re lejano a uC1
 print(uC1.isDirectlyConnectedWith(uC5, 0)) # --> false
 
+# TEST 4°: uC6 no llega a ser directly conn con uC1 por lo que vale la dim 2
+print(uC1.isDirectlyConnectedWith(uC6, 0)) # --> false
 
 
 
+# TESTING FORM CLUSTERS S2 METHOD --> 
+s2 = Stage2(s1ToS2ComQueue, s2ToS1ComQueue)
 
+aL1 = [uC1, uC2, uC4]
+oL1 = []
+uL1 = [aL1, oL1] 
 
-
-
-
-
-#aL = [uC1, ]
-#oL = []
-#uL = [aL, oL] 
-#
-## stages
-#s2 = Stage2(s1ToS2ComQueue, s2ToS1ComQueue, uncommonDimensions=0) # default uncommon dimensions = 0
-#
-#s2.formClusters(uL)
+# TEST 1°: 3 u clusters forman un cluster final por transicion: uc1 -> uc2 -> 4 
+s2.formClusters(uL1) # --> :)
