ee286acb98300469f27163e4c37798bead4cd25c
Camila Onofri <onofricamila2@gmail.com>
2019-10-05 16:56:26 -0300

    parameters config file added

---
+++ parametersConfiguration.py
@@ -0,0 +1,12 @@
+from datasets import dsPilot, dsForming2uCs, dsChoosingClosestReachableUc, dsFromUcToNoise
+from datasets import noisyCirclesDataset
+from datasets import noisyMoonsDataset
+from datasets import blobsDataset
+
+chosenDataset = noisyMoonsDataset
+
+rs = 0.06
+
+tG = 1500
+
+uncdim = 0
